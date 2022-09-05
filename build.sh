55dbde806c605fbd1d55f537909b6adf8a0ac9be
Kacper Kula <kulak@kulak.at>
2022-09-05 23:48:01 +0100

    Adding index title and script to optimise package even further.

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-echo "Removing dist"
-rm -fr dist
+echo "Removing dist and .parcel-cache"
+rm -fr dist .parcel-cache
 echo "renaming classes"
 yarn rename
 echo "building"
@@ -9,14 +9,16 @@ echo "removing source maps"
 rm -fr dist/*.map
 echo "uglify"
 yarn uglify
-echo "removing non-ugly js"
-rm dist/index.*.js
+echo "Roadroller"
+yarn run-roadroller
+echo "removing non-road-roller js"
+rm dist/index.*.js dist/ugly.js
 echo "Replacing file name"
-sed -i .bak 's/index.*.js/ugly.js/' dist/index.html
+sed -i .bak 's/index.*.js/s.js/' dist/index.html
 rm -fr dist/*.bak
 echo "remove old zip"
 rm game.zip
 echo "packing"
-zip -vrj game.zip dist -x "*.DS_Store"
+zip -vrj game.zip dist -x "*.DS_Store" -9
 echo "Recompressing using advzip"
 advzip -z game.zip
